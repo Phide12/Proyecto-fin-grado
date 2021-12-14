ef6abd34744c5eb098b81a3b0196423c03e2a606
jorgemrtr <jorgemrtr>
2021-12-14 12:29:29 +0100

    modificado con composer

--- recursos/javascript/global.js
+++ recursos/javascript/global.js
@@ -1,69 +1,74 @@
 function cargarCabecera() {
-  sideNav = document.getElementById("sideNav");
-  filtroOscuro = document.getElementById('filtro_oscuro');
-  filtroOscuro.style.position = 'fixed';
-  filtroOscuro.style.height = screen.height + 'px';
-  filtroOscuro.addEventListener('click', cerrarMenu);
-  document.getElementById('boton_abrir_menu').addEventListener('click', abrirMenu);
-  document.getElementById('boton_cerrar_menu').addEventListener('click', cerrarMenu);
-  anchura = window.getComputedStyle(sideNav).getPropertyValue('width');
-  cerrarMenu();
-  filtroOscuro.style.transition = '0.5s'
+	sideNav = document.getElementById("sideNav");
+	filtroOscuro = document.getElementById("filtro_oscuro");
+	filtroOscuro.style.position = "fixed";
+	filtroOscuro.style.height = screen.height + "px";
+	filtroOscuro.addEventListener("click", cerrarMenu);
+	document
+		.getElementById("boton_abrir_menu")
+		.addEventListener("click", abrirMenu);
+	document
+		.getElementById("boton_cerrar_menu")
+		.addEventListener("click", cerrarMenu);
+	anchura = window.getComputedStyle(sideNav).getPropertyValue("width");
+	cerrarMenu();
+	filtroOscuro.style.transition = "0.5s";
 }
 
 function abrirMenu() {
-  sideNav.style.left = '0';
-  document.body.style.left = anchura;
-  filtroOscuro.style.opacity = 1;
-  filtroOscuro.style.pointerEvents = 'auto';
+	sideNav.style.left = "0";
+	document.body.style.left = anchura;
+	filtroOscuro.style.opacity = 1;
+	filtroOscuro.style.pointerEvents = "auto";
 }
+
 function cerrarMenu() {
-  sideNav.style.left = '-' + anchura;
-  document.body.style.left = '0';
-  filtroOscuro.style.opacity = 0;
-  filtroOscuro.style.pointerEvents = 'none';
+	sideNav.style.left = "-" + anchura;
+	document.body.style.left = "0";
+	filtroOscuro.style.opacity = 0;
+	filtroOscuro.style.pointerEvents = "none";
 }
 
 cargarCabecera();
-
 /**
- * Aplica efecto de transicion al hacer scroll en los elementos con clase 
+ * Aplica efecto de transicion al hacer scroll en los elementos con clase
  * 'animacion-scroll'
  */
 function cargarAnimacionesScroll() {
-  window.addEventListener('scroll', cargarEnScroll);
-  cargarEnScroll('animacion-scroll');
+	window.addEventListener("scroll", cargarEnScroll);
+	cargarEnScroll("animacion-scroll");
 }
 
 function cargarEnScroll() {
-  let listaElementos = (document.getElementsByClassName('animacion-scroll'));
+	let listaElementos = document.getElementsByClassName("animacion-scroll");
 
-  for (let i = 0; i < listaElementos.length; i++) {
-    let elemento = listaElementos[i];
-    let alturaCarga = window.pageYOffset + window.innerHeight;
-    let alturaElemento = elemento.offsetTop;
+	for (let i = 0; i < listaElementos.length; i++) {
+		let elemento = listaElementos[i];
+		let alturaCarga = window.pageYOffset + window.innerHeight;
+		let alturaElemento = elemento.offsetTop;
 
-    if (alturaCarga >= alturaElemento) {
-      elemento.classList.remove("animacion-scroll");
-      elemento.classList.add("scroll-visible");
-    }
-  }
+		if (alturaCarga >= alturaElemento) {
+			elemento.classList.remove("animacion-scroll");
+			elemento.classList.add("scroll-visible");
+		}
+	}
 }
 
 cargarAnimacionesScroll();
-
 
 /**
  * Scroll automatico
  */
 function cargarAutoScroll() {
-  if (document.getElementById('click-scroll') != null) {
-    document.getElementById('click-scroll').addEventListener('click', autoScroll);    
-  }
+	if (document.getElementById("click-scroll") != null) {
+		document
+			.getElementById("click-scroll")
+			.addEventListener("click", autoScroll);
+	}
 }
 
-function autoScroll() {  
-  window.scrollTo({top: window.innerHeight - 95, behavior: 'smooth'});
+function autoScroll() {
+	window.scrollTo({ top: window.innerHeight - 95, behavior: "smooth" });
 }
 
 cargarAutoScroll();
